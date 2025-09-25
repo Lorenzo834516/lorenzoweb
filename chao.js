027fbcfd341380bf748e6d9a9c249f544e05920f
Lorenzo834516 <l87125909@gmail.com>
2025-09-25 15:32:19 -0400

    Add files via upload

---
+++ chao.js
@@ -0,0 +1,45 @@
+let frutas = ["banana", "manzana", "pera"];
+
+///document.write(frutas [2])
+
+
+
+let amigos = {
+    nombre : "Crisbel",
+    edad : 20,
+    mes : "marzo"
+};
+
+let nombre = amigos["nombre"];
+let edad =  amigos["edad"];
+let mes = amigos["mes"];
+
+
+novia = `el nombre de mi novia es: ${nombre} <br>
+        su edad es de: ${edad} <br> 
+        y su mes es: <b>${mes}</b> <br> `;
+
+document.write(novia);
+
+
+
+
+
+let numeroParaSumar = 0;
+
+
+while (numeroParaSumar < 10) {
+    document.write(numeroParaSumar);
+}
+
+
+
+
+
+
+
+
+
+
+
+
